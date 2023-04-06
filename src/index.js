56ac761f4a4a38d9367a120f9282c053279f6f65
nmorri <nmorri47@uwo.ca>
2023-04-05 22:14:19 -0400

    Tried adding app to index.js

--- src/index.js
+++ src/index.js
@@ -123,6 +123,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
 
